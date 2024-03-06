ccb4c7da8872255719de1a8e999d01a90236129d
thanhhoann <pdth456@gmail.com>
2024-03-06 19:39:24 +0700

    restructure project; add zustand

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Flex, Text, Button } from '@radix-ui/themes'
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <Flex direction="column" gap="2">
       <Text>Hello from Radix Themes :)</Text>
