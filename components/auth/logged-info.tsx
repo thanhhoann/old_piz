f1ad931f88bb113147447e2e911cd70cdafa28ba
thanhhoann <pdth456@gmail.com>
2024-03-12 00:26:26 +0700

    comment out components/auth/logged-info.tsx

--- components/auth/logged-info.tsx
+++ components/auth/logged-info.tsx
@@ -1,8 +1,8 @@
-import { auth } from '@/app/lib/auth'
-import { useSession } from 'next-auth/react'
+// import { auth } from '@/app/lib/auth'
+// import { useSession } from 'next-auth/react'
 import React, { useState, useEffect } from 'react'
 
-export default async function LoggedInfo(props) {
-  const session = await auth()
-  return <>Welcome {session?.user?.name}!</>
+export default async function LoggedInfo() {
+  // const session = await auth()
+  return <></>
 }
